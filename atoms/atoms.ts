f5924909619cbf03d1c59c1731b48b18a12914b5
kevinshen56714 <kevinshen56714@gmail.com>
2024-01-24 15:32:12 -0800

    Add splashscreen

--- atoms/atoms.ts
+++ atoms/atoms.ts
@@ -2,4 +2,4 @@ import {atom} from 'jotai';
 import {Scene} from '../types/scenes';
 
 // Create your atoms and derivatives
-export const sceneAtom = atom(Scene.GAMEBOARD);
+export const sceneAtom = atom(Scene.SPLASHSCREEN);
--- types/scenes.ts
+++ types/scenes.ts
@@ -1,4 +1,5 @@
 export enum Scene {
+  SPLASHSCREEN,
   GAMEBOARD,
   QUESTION,
   FINALJEOPARDY,
