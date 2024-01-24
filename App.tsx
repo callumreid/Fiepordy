b6173d7a3d0ed1ea5b15a2f204fad205ae97e125
Callum Reid <callum.reid@volleythat.com>
2024-01-24 15:19:47 -0800

    add splash screen

--- App.tsx
+++ App.tsx
@@ -2,18 +2,21 @@ import React, {useState} from 'react';
 import GameBoard from './components/GameBoard';
 import FinalJeopardy from './components/FinalJeopardy';
 import Question from './components/Question';
+import SplashScreen from './android/displays/SplashScreen';
 
 export enum Scene {
+  SPLASHSCREEN,
   GAMEBOARD,
   QUESTION,
   FINALJEOPARDY,
 }
 
 export default function App() {
-  const [scene, setScene] = useState<Scene>(Scene.GAMEBOARD);
+  const [scene, setScene] = useState<Scene>(Scene.SPLASHSCREEN);
 
   return (
     <>
+      {scene === Scene.SPLASHSCREEN && <SplashScreen setScene={setScene} />}
       {scene === Scene.GAMEBOARD && <GameBoard setScene={setScene} />}
       {scene === Scene.QUESTION && <Question setScene={setScene} />}
       {scene === Scene.FINALJEOPARDY && <FinalJeopardy />}
