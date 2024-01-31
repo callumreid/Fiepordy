07263daa3d245b51199fda3abf8f5d3da6007b4b
Callum Reid <callum.reid@volleythat.com>
2024-01-31 10:53:49 -0800

    make some fake data

---
+++ src/constants/fakeData.ts
@@ -0,0 +1,113 @@
+export const fakeData = {
+  categories: [
+    {
+      categoryName: 'Cows',
+      categoryDescription: '',
+      clues: [
+        {
+          clue: "A cow's stomach has this many compartments",
+          answer: '4',
+          alsoCorrect: ['four, for, fore'],
+          difficulty: 4,
+          clueID: 'abcd11111',
+        },
+        {
+          clue: 'A cow can produce this many gallons of milk per day',
+          answer: '8',
+          alsoCorrect: ['eight, ate'],
+          difficulty: 2,
+          clueID: 'abcd22222',
+        },
+        {
+          clue: 'Why did the cow cross the road?',
+          answer: 'To get to the udder side',
+          alsoCorrect: ['to get to the other side'],
+          difficulty: 1,
+          clueID: 'abcd33333',
+        },
+      ],
+    },
+    {
+      categoryName: 'Dogs',
+      categoryDescription: 'The answer includes the letters d o g and s',
+      clues: [
+        {
+          clue: 'IAM the plural of dog?',
+          answer: 'dogs',
+          alsoCorrect: ['multi-dog'],
+          difficulty: 1,
+          clueID: 'abcd44444',
+        },
+        {
+          clue: 'Your BLANK have forsaken you',
+          answer: 'Gods',
+          alsoCorrect: ['gods'],
+          difficulty: 3,
+          clueID: 'abcd55555',
+        },
+        {
+          clue: 'A BLANK life',
+          answer: 'dogs',
+          alsoCorrect: ["dog's"],
+          difficulty: 2,
+          clueID: 'abcd66666',
+        },
+      ],
+    },
+    {
+      categoryName: 'Computers',
+      categoryDescription: 'This category is about computers and technology.',
+      clues: [
+        {
+          clue: 'This company created the first personal computer called the Apple I',
+          answer: 'Apple',
+          alsoCorrect: ['apple inc'],
+          difficulty: 3,
+          clueID: 'abcd77777',
+        },
+        {
+          clue: 'This is the standard port number for HTTP',
+          answer: '80',
+          alsoCorrect: ['eighty'],
+          difficulty: 4,
+          clueID: 'abcd88888',
+        },
+        {
+          clue: 'This data structure operates on the Last In, First Out principle',
+          answer: 'Stack',
+          alsoCorrect: ['stacks'],
+          difficulty: 2,
+          clueID: 'abcd99999',
+        },
+      ],
+    },
+    {
+      categoryName: 'Space Exploration',
+      categoryDescription:
+        'This category covers topics related to outer space and its exploration.',
+      clues: [
+        {
+          clue: 'This planet is known as the Red Planet',
+          answer: 'Mars',
+          alsoCorrect: ['mars'],
+          difficulty: 1,
+          clueID: 'abcd101010',
+        },
+        {
+          clue: 'Name of the first man-made object to land on the Moon',
+          answer: 'Luna 2',
+          alsoCorrect: ['luna-2', 'luna ii'],
+          difficulty: 4,
+          clueID: 'abcd111111',
+        },
+        {
+          clue: 'This term describes the boundary around a black hole from which nothing can escape',
+          answer: 'Event Horizon',
+          alsoCorrect: ['event horizon'],
+          difficulty: 3,
+          clueID: 'abcd121212',
+        },
+      ],
+    },
+  ],
+};
